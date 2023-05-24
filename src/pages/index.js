9ba162385d072b656ffb4581b89a50d5bb3bb508
Yuvadi29 <adityatrivedi612@gmail.com>
2023-05-24 16:41:04 +0530

    Getting Started with file and folder structures

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      <center className="grid place-items-center mt-10">
+        <h1 className="text-5xl">Getting Started</h1>
+        
+      </center>
+      {/* ---- ---- */}
+    </div>
+  );
+}
