2b7b861529da377bdc292dd7a9831c4df4262aaa
Yuvadi29 <adityatrivedi612@gmail.com>
2023-06-02 16:55:24 +0530

    Started With WebHooks to Store Data in Firbase after payment on Stripe has been Done

---
+++ src/pages/api/webhook.js
@@ -0,0 +1,12 @@
+import { buffer } from "micro/types/src/lib";
+import * as admin from "firebase-admin";
+
+// Here we will be storing our data in the database after the stripe payment has been successfully done. This is done using webhooks in JavaScript.
+
+
+
+export default async (req, res) => {
+    if (req.method === 'POST'){
+
+    }
+}
