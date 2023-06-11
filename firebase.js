abe38b48c0ff312ccdb5e134bdae904dc0ebf277
Yuvadi29 <adityatrivedi612@gmail.com>
2023-06-11 17:37:22 +0530

    My Orders Page started but will have to complete the Webhooks part first to initialise with the Orders Page Backend

--- firebase.js
+++ firebase.js
@@ -1,12 +1,17 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase";
+import firebase from "./firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD1z5LL0y1GY28CIBGPdEek4GFERe51N80",
-    authDomain: "clone-388116.firebaseapp.com",
-    projectId: "amazonclone-388116",
-    storageBucket: "amazonclone-388116.appspot.com",
-    messagingSenderId: "838843467207",
-    appId: "1:838843467207:web:023f0716b04d8d750745e2",
-    measurementId: "G-8D7S5K5CQN"
-  };
+  apiKey: "AIzaSyD1z5LL0y1GY28CIBGPdEek4GFERe51N80",
+  authDomain: "clone-388116.firebaseapp.com",
+  projectId: "amazonclone-388116",
+  storageBucket: "amazonclone-388116.appspot.com",
+  messagingSenderId: "838843467207",
+  appId: "1:838843467207:web:023f0716b04d8d750745e2",
+  measurementId: "G-8D7S5K5CQN"
+};
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+const db = app.firestore();
+
+export default db;
