157a7ad013492965fc8e8eb656e225cae7b5c310
Yuvadi29 <adityatrivedi612@gmail.com>
2023-07-23 18:56:47 +0530

    Final Build2

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -26,8 +26,8 @@ const Order = ({ id, amount, items, timestamp, images }) => {
 
       <div className="p-5 sm:p-10">
         <div className="flex space-x-6 overflow-x-auto">
-          {images?.map((image) => (
-            <img src={image} alt="product image" className='h-20 object-contain sm:h-32' />
+          {images?.map((image,id) => (
+            <img key ={id} src={image} alt="product image" className='h-20 object-contain sm:h-32' />
           ))}
         </div>
       </div>
