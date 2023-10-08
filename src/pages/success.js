1c846fd5e5ccf7ea11649ecbd74cb82e7aeda650
Yuvadi29 <adityatrivedi612@gmail.com>
2023-10-08 22:13:42 +0530

    Confirmation mail Sent ✅

--- src/pages/success.js
+++ src/pages/success.js
@@ -9,13 +9,26 @@ import { useRef } from 'react';
 
 const Success = () => {
     const form = useRef();
-    const session = useSession();
-    console.log(session?.data?.user?.email);
+    const {data: session} = useSession();
+    const router = useRouter();
 
     const sendMail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_qe5etz8', 'template_foiap3x', form.current, 'hpVPfQVpaWKVjMk2D')
+        const userEmail = session?.data?.user?.email;
+
+        // // Populate the form fields
+        // form.current.querySelector('[name="to_email"]').value = userEmail;
+        // form.current.querySelector('[name="subject"]').value = 'Your Order Confirmation';
+        // form.current.querySelector('[name="message"]').value = 'Thank you for your order!';
+
+        const templateParams = {
+            to_name: session?.user?.name,
+            to_email: session?.user?.email,
+            from_name: 'Amazon',
+        }
+
+        emailjs.send('service_qe5etz8', 'template_foiap3x', templateParams, 'hpVPfQVpaWKVjMk2D')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
@@ -23,7 +36,6 @@ const Success = () => {
             });
     };
 
-    const router = useRouter();
     return (
         <div className='bg-[#EAEDED] h-screen'>
             <Header />
@@ -36,10 +48,13 @@ const Success = () => {
                     </div>
 
                     <div>
-                        Thank you for shopping. You will receive a confirmation mail on your registered email-id when the item has shipped. If you want to check the status of your order, press the link below.
+                        Thank you <b>{session?.user?.name}</b> for shopping. You will receive a confirmation mail on your registered email-id when the item has shipped. If you want to check the status of your order, press the link below.
                     </div>
                     <button className='button mt-8' onClick={() => router.push("/orders")}>Go to My Orders</button>
                     <form ref={form} onSubmit={sendMail}>
+                        <input type='hidden' name='to_email' />
+                        <input type='hidden' name='subject' />
+                        <input type='hidden' name='message' />
                         <a href='https://mail.google.com/' target='_blank'>
                             <button className='button mt-8'>Check Mail</button>
                         </a>
